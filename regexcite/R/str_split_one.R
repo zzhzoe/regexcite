81551867189c96738d656a9067d122ab469ea3c8
zzhzoe <zoe.zzh121@gmail.com>
2022-01-21 02:21:33 -0800

    update test

--- regexcite/R/str_split_one.R
+++ regexcite/R/str_split_one.R
@@ -1,3 +1,18 @@
+#' Split a string
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' str_split_one(y, pattern = stringr::fixed("."))
 str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
