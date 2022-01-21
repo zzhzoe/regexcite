6ce17da2fb07c135c4b8103be93304527f3811f2
zzhzoe <zoe.zzh121@gmail.com>
2022-01-21 02:20:15 -0800

    update

---
+++ regexcite/R/str_split_one.R
@@ -0,0 +1,8 @@
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
+}
